53c9d2b55a6eacd5a396ebd029e9b7dbfd28c970
Roshan Pathak <roshan.pathak_ug25@ashoka.edu.in>
2023-11-21 19:06:24 +0530

    learnt exception handling

---
+++ Basics/ExceptionsHandling.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val arr = arrayOf(1,2,3)
+    try{
+        println(arr[5])
+    }
+    catch(ex: NullPointerException){
+        
+    }
+    catch(e: Exception){
+        println("please check the array index")
+    }
+    finally{
+        println("I will execute no matter what")
+    }
+
+    println("this will not run")
+}
--- Basics/breakAndContinue.kt
+++ Basics/breakAndContinue.kt
@@ -1,8 +1,27 @@
 fun main(){
-    for( number in 1..10){
+    for( number in 1..6){
         if(number== 5){
             continue
         }
         println(number)
     }
+    for( number in 1..10){
+        if(number== 5){
+            break
+        }
+        println(number)
+    }
+    println("End App")
+
+    loop@ for(number in 1..10){
+        println("number: $number")
+        for(i in 1..7){
+            println("innerLoop: $i")
+            if(i == 5){
+                break@loop
+            }
+        }
+    }
+
+    println("End App")
 }
