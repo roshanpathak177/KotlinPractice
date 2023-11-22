0062482679e39fd8204a10654cf723562d350899
Roshan Pathak <roshan.pathak_ug25@ashoka.edu.in>
2023-11-22 07:06:15 +0530

    Added few lines of code

--- Basics/ExceptionsHandling.kt
+++ Basics/ExceptionsHandling.kt
@@ -4,7 +4,7 @@ fun main(){
         println(arr[5])
     }
     catch(ex: NullPointerException){
-        
+        println("pointer is null")
     }
     catch(e: Exception){
         println("please check the array index")
@@ -14,4 +14,17 @@ fun main(){
     }
 
     println("this will not run")
+
+    createUserList(5)
+    createUserList(-3)
+
+
+}
+fun createUserList(count: Int){
+    if(count<0){
+        //raise exception
+        throw IllegalArgumentException("count must be greater than 0")
+    } else{
+        println("user list created containing $count users")
+    }
 }
